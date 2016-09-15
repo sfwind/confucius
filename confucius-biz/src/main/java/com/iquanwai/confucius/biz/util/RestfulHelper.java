782c834a53d9423635e433076c465cd9a58d72eb
Ding Zhi Jun <dzjnamegoal@hotmail.com>
2016-09-15 08:09:22 +0800

    ut

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/util/RestfulHelper.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/util/RestfulHelper.java
@@ -55,49 +55,25 @@ public class RestfulHelper {
 
     public String postXML(String requestUrl, String xml) {
         logger.info("requestUrl: {}\nxml: {}",requestUrl, xml);
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try {
-            URL realUrl = new URL(requestUrl);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("Content-Type","text/xml; charset=utf-8");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(xml);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            logger.error(e.getCause().getMessage(), e);
-        }
-        //使用finally块来关闭输出流、输入流
-        finally{
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(in!=null){
-                    in.close();
-                }
-            }
-            catch(IOException ex){
-                // ignore
+        if(StringUtils.isNotEmpty(requestUrl) && StringUtils.isNotEmpty(xml)) {
+            Request request = new Request.Builder()
+                    .url(requestUrl)
+                    .post(RequestBody.create(XML, xml))
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                String body = response.body().string();
+//                if(CommonUtils.isError(body)){
+//                    logger.error("execute {} return error, error message is {}", requestUrl, body);
+//                }
+                logger.info("body:{}", body);
+                return body;
+            } catch (Exception e) {
+                logger.error(e.getCause().getMessage(), e);
             }
         }
-        return result;
+        return "";
     }
 
 
