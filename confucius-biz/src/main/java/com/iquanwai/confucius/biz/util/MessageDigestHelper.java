15f0fa16b4134aa488fc241e17879f39bbfddb95
Ding Zhi Jun <dzjnamegoal@hotmail.com>
2016-09-16 11:10:03 +0800

    md

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/util/MessageDigestHelper.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/util/MessageDigestHelper.java
@@ -1,6 +1,5 @@
 package com.iquanwai.confucius.biz.util;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -9,35 +8,40 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MessageDigestHelper {
     public static String getSHA1String(String s) {
-        //SHA-1加密实例
-        MessageDigest sha1 = null;
-        try {
-            sha1 = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        sha1.update(s.getBytes());
-        byte[] codedBytes = sha1.digest();
-        //将加密后的字节数组转换成字符串
-        String codedString = new BigInteger(1, codedBytes).toString(16);
-
-        return codedString;
+        return getHash(s, "SHA-1");
     }
 
     public static String getMD5String(String s) {
-        //SHA-1加密实例
-        MessageDigest sha1 = null;
-        try {
-            sha1 = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        sha1.update(s.getBytes());
-        byte[] codedBytes = sha1.digest();
-        //将加密后的字节数组转换成字符串
-        String codedString = new BigInteger(1, codedBytes).toString(16);
+        return getHash(s, "MD5");
+    }
 
-        return codedString.toUpperCase();
+    private static String getHash(String s, String algorithm) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            //ignore
+        }
+        md5.update(s.getBytes());
+        byte[] codedBytes = md5.digest();
+        //将加密后的字节数组转换成字符串
+        return bytesToHexString(codedBytes);
+    }
+
+    private static String bytesToHexString(byte[] src){
+        StringBuilder stringBuilder = new StringBuilder("");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+        for (int i = 0; i < src.length; i++) {
+            int v = src[i] & 0xFF;
+            String hv = Integer.toHexString(v);
+            if (hv.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hv);
+        }
+        return stringBuilder.toString().toUpperCase();
     }
 
 }
