dd68b4d6065dc43f124b2d488dc8b6e968f23e7c
Ding Zhi Jun <dzjnamegoal@hotmail.com>
2016-10-07 09:57:17 +0800

    revert

--- confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/CourseController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/CourseController.java
@@ -36,25 +36,6 @@ public class CourseController {
     @Autowired
     private OperationLogService operationLogService;
 
-
-    @RequestMapping("/isentry/{courseId}")
-    public ResponseEntity<Map<String, Object>> isEntry(LoginUser loginUser,
-                                                       @PathVariable("courseId") Integer courseId){
-        try{
-            Assert.notNull(loginUser,"用户不能为空");
-            ClassMember classMember = courseProgressService.loadActiveCourse(loginUser.getOpenId(), courseId);
-            if(classMember==null){
-                LOGGER.error("用户"+loginUser.getWeixinName()+"还没有报名, openid is {}", loginUser.getOpenId());
-                return WebUtils.result(false);
-            }
-
-            return WebUtils.result(true);
-        }catch (Exception e){
-            LOGGER.error("获取用户报名状态失败", e);
-            return WebUtils.error("获取用户报名状态失败");
-        }
-    }
-
     @RequestMapping("/load")
     public ResponseEntity<Map<String, Object>> loadCourse(LoginUser loginUser){
         try{
@@ -62,7 +43,7 @@ public class CourseController {
             ClassMember classMember = courseProgressService.loadActiveCourse(loginUser.getOpenId(), null);
             if(classMember==null){
                 LOGGER.error("用户"+loginUser.getWeixinName()+"还没有报名, openid is {}", loginUser.getOpenId());
-                return WebUtils.error(220, ErrorMessageUtils.getErrmsg("course.load.nopaid"));
+                return WebUtils.error(ErrorMessageUtils.getErrmsg("course.load.nopaid"));
             }
 
             int week = getProgressWeek(classMember);
