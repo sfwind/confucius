cef25f8026d6ad94ae6d95994ad3445ead6be8ec
nethunder <17089113140@163.com>
2017-01-19 22:02:31 +0800

    反馈地址

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/survey/dto/SurveyResultDto.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/survey/dto/SurveyResultDto.java
@@ -7,16 +7,6 @@ import lombok.Data;
  */
 @Data
 public class SurveyResultDto {
-    /**
-     * {activity=11853325,
-     * name=全测试1,
-     * sojumpparm=4,
-     * q1=1, q2=1,2,
-     * q3=填空, q4_1=姓名, q4_2=21, q4_3=test, q5=2, q6=2,3,
-     * index=2,
-     * timetaken=19,
-     * submittime=2017-01-17 19:26:01}
-     */
     private Integer activity;
     private String name;
     private Integer sojumpparm;
