b7cc3d530baa1f43e054ca2d0797b4a8aa2d081c
nethunder <17089113140@163.com>
2017-01-29 16:57:10 +0800

    fix

--- confucius-web/src/main/java/com/iquanwai/confucius/web/mobile/controller/MobileSurveyController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/mobile/controller/MobileSurveyController.java
@@ -7,7 +7,6 @@ import com.iquanwai.confucius.web.util.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * Created by nethunder on 2017/1/29.
@@ -30,9 +31,12 @@ public class MobileSurveyController {
     public void redirectToSurvey(LoginUser loginUser,
                                  HttpServletRequest request,
                                  HttpServletResponse response,
-                                 @RequestParam("activity") Integer activity) {
-        try {
-            Assert.notNull(loginUser, "用户的登录信息不能为空");
+                                 @RequestParam("activity") Integer activity) throws IOException {
+        if (loginUser == null) {
+            response.sendRedirect(ConfigUtils.adapterDomainName() +
+                    "/static/login/result?err=" +
+                    URLEncoder.encode("您还未关注公众号", "UTF-8"));
+        } else {
             // 插入问卷提交表
             Integer submitId = surveyService.insertSurveySubmit(loginUser.getOpenId(), activity);
             if (submitId != null && submitId > 0) {
@@ -50,14 +54,6 @@ public class MobileSurveyController {
                 logger.error("用户:{},插入问卷:{},提交记录失败", loginUser.getOpenId(), activity);
                 response.sendRedirect("/403.jsp");
             }
-        } catch (Exception e) {
-            logger.error("跳转到问卷页面失败",e);
-            try{
-                response.sendRedirect("/403.jsp");
-            } catch (Exception e1){
-                logger.error("跳转异常页面失败", e);
-            }
         }
-
     }
 }
