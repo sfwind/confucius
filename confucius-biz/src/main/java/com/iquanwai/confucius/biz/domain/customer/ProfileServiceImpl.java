6f8402d8f504c9f10b1e88a43e9be6aaee40ae4d
nethunder <17089113140@163.com>
2017-02-08 18:00:05 +0800

    fix 同步用户表

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/customer/ProfileServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/customer/ProfileServiceImpl.java
@@ -1,7 +1,10 @@
 package com.iquanwai.confucius.biz.domain.customer;
 
 import com.iquanwai.confucius.biz.dao.customer.ProfileDao;
+import com.iquanwai.confucius.biz.dao.wx.FollowUserDao;
+import com.iquanwai.confucius.biz.po.Account;
 import com.iquanwai.confucius.biz.po.customer.Profile;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +20,23 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Autowired
     private ProfileDao profileDao;
+    @Autowired
+    private FollowUserDao followUserDao;
 
     @Override
     public Profile getProfile(String openId) {
-        return profileDao.queryByOpenId(openId);
+        Profile profile =  profileDao.queryByOpenId(openId);
+        if(profile==null){
+            // 查询一下account
+            Account account = followUserDao.queryByOpenid(openId);
+            if(account!=null){
+                profile = new Profile();
+                ModelMapper modelMapper = new ModelMapper();
+                modelMapper.map(account, profile);
+                profileDao.insertProfile(profile);
+            }
+        }
+        return profile;
     }
 
     @Override
@@ -28,4 +44,5 @@ public class ProfileServiceImpl implements ProfileService {
         Assert.notNull(profile.getOpenid(), "openID不能为空");
         profileDao.submitPersonalCenterProfile(profile);
     }
+
 }
