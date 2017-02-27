bc341765455ad38e6129b9a70e88b650c79c0d58
nethunder <17089113140@163.com>
2017-02-27 14:30:53 +0800

    拦截

--- confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
@@ -79,6 +79,16 @@ public class IndexController {
         return courseView(request);
     }
 
+    @RequestMapping(value = "/pay/signup",method = RequestMethod.GET)
+    public ModelAndView getSignupIndex(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        if(!checkAccessToken(request)){
+            CookieUtils.removeCookie(OAuthService.ACCESS_TOKEN_COOKIE_NAME, response);
+            WebUtils.auth(request, response);
+            return null;
+        }
+        return courseView(request);
+    }
+
     @RequestMapping(value = "/personal/edit",method = RequestMethod.GET)
     public ModelAndView getPersonalEditIndex(HttpServletRequest request, HttpServletResponse response) throws Exception{
         if(!checkAccessToken(request)){
