0c58bb862b8db2cf7a06095191a790d2369cc367
nethunder <17089113140@163.com>
2017-03-15 18:54:04 +0800

    fix pc端提交记录数据

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/fragmentation/controller/FragmentController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/fragmentation/controller/FragmentController.java
@@ -408,6 +408,8 @@ public class FragmentController {
             });
 
             show.setLabelList(labelConfigs);
+            practiceService.riseArticleViewCount(Constants.ViewInfo.Module.SUBJECT, submitId,Constants.ViewInfo.EventType.PC_SHOW);
+
             return WebUtils.result(show);
         }
     }
