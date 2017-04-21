06e58e9b5214c378eb259722e744b5945ee335bc
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-04-21 14:27:17 +0800

    index

--- confucius-web/src/main/java/com/iquanwai/confucius/web/PCIndexController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/PCIndexController.java
@@ -52,7 +52,7 @@ public class PCIndexController {
      */
     @RequestMapping(value = {"/home", "/"}, method = RequestMethod.GET)
     public ModelAndView getHome(HttpServletRequest request, PCLoginUser pcLoginUser) {
-        return ConfigUtils.isDevelopment() ? new ModelAndView("index") : pcView(request, pcLoginUser);
+        return ConfigUtils.isDevelopment() ? new ModelAndView("index") : new ModelAndView("home");
     }
 
     @RequestMapping(value = "/servercode")
@@ -74,7 +74,7 @@ public class PCIndexController {
     }
 
     private ModelAndView pcView(HttpServletRequest request,PCLoginUser pcLoginUser) {
-        ModelAndView mav = new ModelAndView("home");
+        ModelAndView mav = new ModelAndView("site");
         if(ConfigUtils.isPcMaintenance()){
             // 正在维护
             mav = new ModelAndView("maintenance");
