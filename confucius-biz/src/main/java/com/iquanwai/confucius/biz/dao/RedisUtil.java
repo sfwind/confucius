f73ebf59af70935b7a59f8d6aa117a5c09858d3c
nethunder <17089113140@163.com>
2017-04-27 11:55:02 +0800

    插入followuser表，加锁

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/RedisUtil.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/RedisUtil.java
@@ -1,16 +1,24 @@
 package com.iquanwai.confucius.biz.dao;
 
 import org.redisson.api.RBucket;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * Created by nethunder on 2017/4/26.
  */
+
 @Repository
 public class RedisUtil {
     private RedissonClient redissonClient;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public void setRedissonClient(RedissonClient redissonClient){
@@ -33,5 +41,20 @@ public class RedisUtil {
     public <T> void set(String key, T value) {
         RBucket<T> bucket = redissonClient.getBucket(key);
         bucket.set(value);
+    }
+
+    public void lock(String key, Consumer<RLock> consumer) {
+        RLock lock = redissonClient.getLock(key);
+        logger.info("Thread {} want the lock", Thread.currentThread().getId());
+        try {
+            lock.tryLock(60, 10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.info("Thread {} has lock :{}", Thread.currentThread().getId(), lock.isHeldByCurrentThread());
+        consumer.accept(lock);
+        logger.info("Thread {} will release the lock",Thread.currentThread().getId());
+        lock.unlock();
+        logger.info("Thread {} don't have the lock :{}", Thread.currentThread().getId(), lock.isHeldByCurrentThread());
     }
 }
