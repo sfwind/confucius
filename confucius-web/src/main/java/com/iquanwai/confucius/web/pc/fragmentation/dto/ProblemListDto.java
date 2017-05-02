377ef519dd663c71280c3340a850444d1e1c484c
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-02 11:19:55 +0800

    update asst coach

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/fragmentation/dto/ProblemListDto.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/fragmentation/dto/ProblemListDto.java
@@ -11,4 +11,5 @@ public class ProblemListDto {
     private String problem;// 问题
     private Integer status; // 1 进行中  2  已完成  3 已过期 -1 未解锁
     private Boolean del; //是否删除
+    private Boolean trial; //是否试用
 }
