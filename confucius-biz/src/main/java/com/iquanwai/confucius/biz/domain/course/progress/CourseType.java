ba9c35a91e533bd9db357753efab00d2f06f1b5f
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-20 00:57:22 +0800

    update

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/course/progress/CourseType.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/course/progress/CourseType.java
@@ -24,29 +24,29 @@ public class CourseType {
     public static final Map<Integer, String> iconCompleteMap = Maps.newHashMap();
 
     static{
-        iconLockMap.put(CHALLENGE, PICURL_PREFIX+"锁定图标.png");
-        iconLockMap.put(HOMEWORK, PICURL_PREFIX+"锁定图标.png");
-        iconLockMap.put(ASSESSMENT, PICURL_PREFIX+"群点评锁定图标.png");
-        iconLockMap.put(RELAX, PICURL_PREFIX+"休息日图标.png");
-        iconLockMap.put(GRADUATE, PICURL_PREFIX+"毕业锁定图标.png");
-        iconLockMap.put(NEW_CHALLENGE, PICURL_PREFIX+"锁定图标.png");
-        iconLockMap.put(NEW_HOMEWORK, PICURL_PREFIX+"锁定图标.png");
+        iconLockMap.put(CHALLENGE, PICURL_PREFIX+"course_locked.png");
+        iconLockMap.put(HOMEWORK, PICURL_PREFIX+"course_locked.png");
+        iconLockMap.put(ASSESSMENT, PICURL_PREFIX+"group_talk_locked.png");
+        iconLockMap.put(RELAX, PICURL_PREFIX+"relax_day.png");
+        iconLockMap.put(GRADUATE, PICURL_PREFIX+"graduate_locked.png");
+        iconLockMap.put(NEW_CHALLENGE, PICURL_PREFIX+"course_locked.png");
+        iconLockMap.put(NEW_HOMEWORK, PICURL_PREFIX+"course_locked.png");
 
-        iconUnLockMap.put(CHALLENGE, PICURL_PREFIX+"待完成图标.png");
-        iconUnLockMap.put(HOMEWORK, PICURL_PREFIX+"待完成图标.png");
-        iconUnLockMap.put(ASSESSMENT, PICURL_PREFIX+"群点评解锁图标.png");
-        iconUnLockMap.put(RELAX, PICURL_PREFIX+"休息日图标.png");
-        iconUnLockMap.put(GRADUATE, PICURL_PREFIX+"毕业解锁图标.png");
-        iconUnLockMap.put(NEW_CHALLENGE, PICURL_PREFIX+"待完成图标.png");
-        iconUnLockMap.put(NEW_HOMEWORK, PICURL_PREFIX+"待完成图标.png");
+        iconUnLockMap.put(CHALLENGE, PICURL_PREFIX+"course_incomplete.png");
+        iconUnLockMap.put(HOMEWORK, PICURL_PREFIX+"course_incomplete.png");
+        iconUnLockMap.put(ASSESSMENT, PICURL_PREFIX+"group_talk_unlock.png");
+        iconUnLockMap.put(RELAX, PICURL_PREFIX+"relax_day.png");
+        iconUnLockMap.put(GRADUATE, PICURL_PREFIX+"graduate_unlock.png");
+        iconUnLockMap.put(NEW_CHALLENGE, PICURL_PREFIX+"course_incomplete.png");
+        iconUnLockMap.put(NEW_HOMEWORK, PICURL_PREFIX+"course_incomplete.png");
 
-        iconCompleteMap.put(CHALLENGE, PICURL_PREFIX+"完成图标.png");
-        iconCompleteMap.put(HOMEWORK, PICURL_PREFIX+"完成图标.png");
-        iconCompleteMap.put(ASSESSMENT, PICURL_PREFIX+"群点评锁定图标.png");
-        iconCompleteMap.put(RELAX, PICURL_PREFIX+"休息日图标.png");
-        iconCompleteMap.put(GRADUATE, PICURL_PREFIX+"毕业锁定图标.png");
-        iconCompleteMap.put(NEW_CHALLENGE, PICURL_PREFIX+"完成图标.png");
-        iconCompleteMap.put(NEW_HOMEWORK, PICURL_PREFIX+"完成图标.png");
+        iconCompleteMap.put(CHALLENGE, PICURL_PREFIX+"course_locked.png");
+        iconCompleteMap.put(HOMEWORK, PICURL_PREFIX+"course_locked.png");
+        iconCompleteMap.put(ASSESSMENT, PICURL_PREFIX+"group_talk_locked.png");
+        iconCompleteMap.put(RELAX, PICURL_PREFIX+"relax_day.png");
+        iconCompleteMap.put(GRADUATE, PICURL_PREFIX+"graduate_locked.png");
+        iconCompleteMap.put(NEW_CHALLENGE, PICURL_PREFIX+"course_complete.png");
+        iconCompleteMap.put(NEW_HOMEWORK, PICURL_PREFIX+"course_complete.png");
     }
 
     public static String getUrl(int type, boolean unlock, boolean complete){
