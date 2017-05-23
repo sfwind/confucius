86292f9e2af4428b5d61a3532482fb712c86c823
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-23 17:48:06 +0800

    update

--- confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
@@ -142,7 +142,9 @@ public class IndexController {
 
         Account account = accountService.getAccount(openId, false);
         if (account != null) {
+            logger.info("用户未关注, {}", account);
             if (account.getSubscribe() != null && account.getSubscribe() == 0) {
+                logger.info("用户未关注, {}", openId);
                 // 未关注
                 try {
                     response.sendRedirect(ConfigUtils.adapterDomainName() + "/static/subscribe");
