19c89faa1b9b8597315ee7976ceca0a774d24ad7
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-27 15:14:43 +0800

    asst role check fix

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/po/common/permisson/Role.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/po/common/permisson/Role.java
@@ -23,9 +23,9 @@ public class Role {
     public static final Integer COACH_LEVEL_3 = 5; //高级教练
     public static final Integer COACH_LEVEL_4 = 6; //首席教练
     public static final Integer BIG_VIP = 7; //大V
-    public static final Integer CONTENT_OPERATION = 8; //内容管理员
-    public static final Integer CONTENT_ADMIN = 9; //管理员
-    public static final Integer ADMIN = 10; //内容运营
+    public static final Integer CONTENT_ADMIN = 8; //内容管理员
+    public static final Integer ADMIN = 9; //管理员
+    public static final Integer CONTENT_OPERATION = 10; //内容运营
 
     private static List<Integer> asstRoleLevel = Lists.newArrayList(COACH_LEVEL_1, COACH_LEVEL_2,
             COACH_LEVEL_3, COACH_LEVEL_4, CONTENT_OPERATION, CONTENT_ADMIN);
