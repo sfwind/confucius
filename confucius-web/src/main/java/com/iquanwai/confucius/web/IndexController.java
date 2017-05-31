006114c5c442e24807a900e9bdeb969ef0303dda
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-31 12:32:31 +0800

    get user info

--- confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/IndexController.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -233,7 +232,7 @@ public class IndexController {
         }
 
         try {
-            accountService.getAccount(openId, false);
+            accountService.getAccount(openId, true);
         } catch (NotFollowingException e) {
             accountService.savePromotionUser(openId, source);
             return true;
