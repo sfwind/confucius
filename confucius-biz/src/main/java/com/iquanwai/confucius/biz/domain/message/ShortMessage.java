4ac8572abd31425b24c338627638699eb3d9eb41
nethunder <17089113140@163.com>
2017-06-18 01:18:08 +0800

    update log

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/message/ShortMessage.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/message/ShortMessage.java
@@ -1,6 +1,8 @@
 package com.iquanwai.confucius.biz.domain.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
@@ -9,9 +11,12 @@ import java.util.Map;
  * Created by nethunder on 2017/6/15.
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShortMessage {
     private Integer profileId;
     private List<String> phones;
     private String content;
     private Map<String,String> replace;
+
 }
--- confucius-web/src/main/java/com/iquanwai/confucius/web/operation/OperationController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/operation/OperationController.java
@@ -1,11 +1,21 @@
 package com.iquanwai.confucius.web.operation;
 
 import com.iquanwai.confucius.biz.domain.log.OperationLogService;
+import com.iquanwai.confucius.biz.domain.message.SMSSendResult;
+import com.iquanwai.confucius.biz.domain.message.ShortMessage;
+import com.iquanwai.confucius.biz.domain.message.ShortMessageService;
 import com.iquanwai.confucius.biz.domain.operation.OperationService;
+import com.iquanwai.confucius.biz.po.OperationLog;
+import com.iquanwai.confucius.web.account.dto.SMSDto;
 import com.iquanwai.confucius.web.resolver.LoginUser;
 import com.iquanwai.confucius.web.util.WebUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +28,14 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/operation")
 public class OperationController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private OperationService operationService;
     @Autowired
     private OperationLogService operationLogService;
+    @Autowired
+    private ShortMessageService shortMessageService;
 
     @RequestMapping(value = "discount", method = RequestMethod.GET)
     public ResponseEntity<Map<String, Object>> getDiscountValue(LoginUser loginUser) {
@@ -61,4 +74,30 @@ public class OperationController {
         return WebUtils.success();
     }
 
+    @RequestMapping(value = "send/message", method = RequestMethod.POST)
+    public ResponseEntity<Map<String,Object>> sendMessage(LoginUser loginUser, @RequestBody SMSDto smsDto){
+        Assert.notNull(loginUser);
+        OperationLog operationLog = OperationLog.create()
+                .module("运营")
+                .function("短信")
+                .action("发送短信")
+                .memo(loginUser.getId() + "");
+        ShortMessage shortMessage = new ShortMessage(loginUser.getId(),
+                smsDto.getPhones(), smsDto.getContent(), smsDto.getReplace());
+        Pair<Integer, Integer> checkSendAble = shortMessageService.checkSendAble(shortMessage);
+        if (checkSendAble.getLeft() > 0) {
+            SMSSendResult smsSendResult = shortMessageService.sendMessage(shortMessage);
+            if (smsSendResult != null && "0".equals(smsSendResult.getResult())) {
+                // 发送成功
+                shortMessageService.raiseSendCount(loginUser.getId());
+            } else {
+                logger.info("发送失败:{}", smsDto);
+                return WebUtils.error("发送失败" + (smsSendResult != null ? (":" + smsSendResult.getDesc()) : ""));
+            }
+            return WebUtils.success();
+        } else {
+            logger.info("用户:{}，暂时不能发送:{}", loginUser.getId(), checkSendAble);
+            return WebUtils.error("暂时不能发送，请稍后再发");
+        }
+    }
 }
