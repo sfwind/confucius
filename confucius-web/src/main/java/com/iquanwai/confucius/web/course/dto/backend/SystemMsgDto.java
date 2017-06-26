381346deb073977e9706dda0607fe0460aaac914
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-06-26 15:44:43 +0800

    add system message backend

---
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/course/dto/backend/SystemMsgDto.java
@@ -0,0 +1,14 @@
+package com.iquanwai.confucius.web.course.dto.backend;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by justin on 17/6/26.
+ */
+@Data
+public class SystemMsgDto {
+    private List<Integer> profileIds;
+    private String message;
+}
