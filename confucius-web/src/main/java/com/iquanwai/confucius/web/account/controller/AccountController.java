fa6e5eb91d1254d2da2d9d80eab2997f3311b3ae
xfduan <1047210087@qq.com>
2017-07-04 15:15:35 +0800

    1

--- confucius-web/src/main/java/com/iquanwai/confucius/web/account/controller/AccountController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/account/controller/AccountController.java
@@ -151,12 +151,12 @@ public class AccountController {
 
     @RequestMapping(value = "/check/follow")
     public ResponseEntity<Map<String, Object>> checkIsFollow(HttpServletRequest request, LoginUser loginUser) {
-        System.out.println("loginUser = " + loginUser.getId());
+        logger.info("loginUser:" + loginUser.getId());
         String cookie = CookieUtils.getCookie(request, OAuthService.QUANWAI_TOKEN_COOKIE_NAME);
-        System.out.println("cookie = " + cookie);
+        logger.info("cookie" + cookie);
         if(cookie != null) {
             boolean isFollowing = loginUserService.userIsFollowing(loginUser);
-            System.out.println("isFollowing = " + isFollowing);
+            logger.info("isFollowing" + isFollowing);
             if(isFollowing) {
                 return WebUtils.success();
             } else {
