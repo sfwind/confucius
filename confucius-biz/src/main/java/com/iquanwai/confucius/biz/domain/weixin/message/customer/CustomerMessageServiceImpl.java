34e1879d66208823d3eebacce6b73c6d188faf2a
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-07-08 22:02:46 +0800

    update

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/message/customer/CustomerMessageServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/message/customer/CustomerMessageServiceImpl.java
@@ -2,6 +2,8 @@ package com.iquanwai.confucius.biz.domain.weixin.message.customer;
 
 import com.google.gson.Gson;
 import com.iquanwai.confucius.biz.util.RestfulHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +15,27 @@ public class CustomerMessageServiceImpl implements CustomerMessageService {
     @Autowired
     private RestfulHelper restfulHelper;
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public void sendCustomerMessage(String openid, String message, MessageType type) {
         if (type.equals(MessageType.TEXT)) {
             TextCustomerMessage customerMessage = new TextCustomerMessage(openid, message);
             Gson gson = new Gson();
             String json = gson.toJson(customerMessage);
+            logger.info("json is {}", json);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         } else if(type.equals(MessageType.IMAGE)){
             ImageCustomerMessage customerMessage = new ImageCustomerMessage(openid, message);
             Gson gson = new Gson();
             String json = gson.toJson(customerMessage);
+            logger.info("json is {}", json);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         } else if(type.equals(MessageType.VOICE)){
             VoiceCustomerMessage customerMessage = new VoiceCustomerMessage(openid, message);
             Gson gson = new Gson();
             String json = gson.toJson(customerMessage);
+            logger.info("json is {}", json);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         }
     }
