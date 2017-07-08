baad899c9dc8a00b7840ab7996c4d8871cef0f60
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-07-08 09:20:20 +0800

    remove ecrypt code

--- confucius-web/src/main/java/com/iquanwai/confucius/web/weixin/CallbackMessageController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/weixin/CallbackMessageController.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -58,7 +57,7 @@ public class CallbackMessageController {
                 + ", nonce is " + nonce + " , echostring is " + echostring);
         String token = ConfigUtils.getToken();
         //对请求参数和自己的token进行排序，并连接排序后的结果为一个字符串
-        String[] strSet = new String[]{token, timestamp, nonce, echostring};
+        String[] strSet = new String[]{token, timestamp, nonce};
         Arrays.sort(strSet);
         String total = "";
         for (String string : strSet) {
@@ -75,21 +74,7 @@ public class CallbackMessageController {
             LOGGER.info("codedString is " + codedString);
             //将加密的结果与请求参数中的signature比对，如果相同，原样返回echostr参数内容
             if (codedString.equals(signature)) {
-                Result result;
-                try {
-                    LOGGER.info("aeskey is " + ConfigUtils.getEncodingAESKey());
-                    Prpcrypt crypt = new Prpcrypt(ConfigUtils.getEncodingAESKey());
-                    result = crypt.decrypt(echostring, "");
-                } catch (Exception e) {
-                    LOGGER.error("decrypt error", e);
-                    return INVALID_DECRYPT;
-                }
-                if (result != null && !result.getResult().isEmpty()) {
-                    LOGGER.info("result is " + result.getResult());
-                    return result.getResult();
-                } else if (result != null) {
-                    LOGGER.info("error code is " + result.getCode());
-                }
+                return echostring;
             }
         } catch (NoSuchAlgorithmException e) {
             LOGGER.error("NoSuchAlgorithmException", e);
@@ -115,28 +100,28 @@ public class CallbackMessageController {
             is.close();
 
             //获取请求参数
-            String signature = request.getParameter("msg_signature");
-            String timestamp = request.getParameter("timestamp");
-            String nonce = request.getParameter("nonce");
-            if (signature == null || timestamp == null ||
-                    nonce == null) {
-                return INVALID_REQUEST;
-            }
-
-            // 加密xml
-            String encryptedXml = xml;
-            // weixin token
-            String token = ConfigUtils.getToken();
-
-            Result result = Prpcrypt.fromTencent(encryptedXml, ConfigUtils.getEncodingAESKey(), signature, token, timestamp, nonce, "");
-            if (result.getCode() != 0) {
-                return INVALID_REQUEST;
-            }
-
-            String decryptedXml = result.getResult();
-            LOGGER.info("decrypted xml is \n" + decryptedXml);
-            Document decryptedDocument = XMLHelper.convertStringToDocument(decryptedXml);
-            callbackMessageService.handleCallback(decryptedDocument);
+//            String signature = request.getParameter("msg_signature");
+//            String timestamp = request.getParameter("timestamp");
+//            String nonce = request.getParameter("nonce");
+//            if (signature == null || timestamp == null ||
+//                    nonce == null) {
+//                return INVALID_REQUEST;
+//            }
+//
+//            // 加密xml
+//            String encryptedXml = xml;
+//            // weixin token
+//            String token = ConfigUtils.getToken();
+//
+//            Result result = Prpcrypt.fromTencent(encryptedXml, ConfigUtils.getEncodingAESKey(), signature, token, timestamp, nonce, "");
+//            if (result.getCode() != 0) {
+//                return INVALID_REQUEST;
+//            }
+//
+//            String decryptedXml = result.getResult();
+            LOGGER.info("xml is \n" + xml);
+//            Document decryptedDocument = XMLHelper.convertStringToDocument(decryptedXml);
+            callbackMessageService.handleCallback(document);
         } catch (Exception e) {
             LOGGER.error("received user message failed", e);
         }
