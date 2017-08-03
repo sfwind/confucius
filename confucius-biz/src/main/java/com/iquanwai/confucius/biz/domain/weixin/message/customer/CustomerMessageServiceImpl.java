3653c6c2c487e9132496c98a261739255cd75c0d
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-08-03 16:48:34 +0800

    escape html

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/message/customer/CustomerMessageServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/message/customer/CustomerMessageServiceImpl.java
@@ -1,10 +1,9 @@
 package com.iquanwai.confucius.biz.domain.weixin.message.customer;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.iquanwai.confucius.biz.util.Constants;
 import com.iquanwai.confucius.biz.util.RestfulHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,23 +15,21 @@ public class CustomerMessageServiceImpl implements CustomerMessageService {
     @Autowired
     private RestfulHelper restfulHelper;
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     @Override
     public void sendCustomerMessage(String openid, String message, Integer type) {
         if (Constants.WEIXIN_MESSAGE_TYPE.TEXT == type) {
             TextCustomerMessage customerMessage = new TextCustomerMessage(openid, message);
-            Gson gson = new Gson();
+            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
             String json = gson.toJson(customerMessage);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         } else if(Constants.WEIXIN_MESSAGE_TYPE.IMAGE == type){
             ImageCustomerMessage customerMessage = new ImageCustomerMessage(openid, message);
-            Gson gson = new Gson();
+            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
             String json = gson.toJson(customerMessage);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         } else if(Constants.WEIXIN_MESSAGE_TYPE.VOICE == type){
             VoiceCustomerMessage customerMessage = new VoiceCustomerMessage(openid, message);
-            Gson gson = new Gson();
+            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
             String json = gson.toJson(customerMessage);
             restfulHelper.post(SEND_CUSTOMER_MESSAGE_URL, json);
         }
