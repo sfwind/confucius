5da15f6fd1daa3fffcb34c3b7bcbabac54d4a85d
nethunder <17089113140@163.com>
2017-08-10 15:54:26 +0800

    update

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/message/MessageService.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/message/MessageService.java
@@ -1,10 +1,21 @@
 package com.iquanwai.confucius.biz.domain.message;
 
 
+import java.util.Date;
+
 /**
  * Created by justin on 17/2/27.
  */
 public interface MessageService {
+
+    /**
+     * 记录发送模版消息的情况
+     * @param openId openid
+     * @param publishTime 发送时间
+     * @param comment 备注
+     */
+    void logCustomerMessage(String openId, Date publishTime, String comment);
+
     /**
      * 评论区回复消息
      * @param message 消息
@@ -16,5 +27,13 @@ public interface MessageService {
 
     String SYSTEM_MESSAGE ="AUTO";
 
+    /**
+     * 发送报警消息
+     * @param alarmTitle 报警标题
+     * @param alarmTips 报警信息
+     * @param alarmLevel 报警级别
+     * @param desc 描述
+     * @param exception 异常信息
+     */
     void sendAlarm(String alarmTitle, String alarmTips, String alarmLevel, String desc, String exception);
 }
