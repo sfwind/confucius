37af8c5f408eaef96da564033c481b2c4243eb8f
nethunder <17089113140@163.com>
2017-09-04 19:06:58 +0800

    update

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/survey/SurveyService.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/survey/SurveyService.java
@@ -14,15 +14,38 @@ public interface SurveyService {
     String PC_PREFIX = "https://www.iquanwai.com/pc/survey/wjx?activity=";
     String MOBILE_PREFIX = "https://www.iquanwai.com/survey/wjx?activity=";
 
+    /**
+     * 插入问卷链接
+     */
     Integer insertSurveySubmit(String openId, Integer activity);
 
+    /**
+     * 提交问卷
+     * @param submit 问卷提交数据
+     * @return 提交结果
+     */
     Boolean submitSurvey(SurveySubmit submit);
 
+    /**
+     * 加载问卷
+     * @param id submitId
+     */
     SurveySubmit load(Integer id);
 
+    /**
+     * 批量插入问题
+     * @param submits 提交内容
+     */
     void batchSubmitQuestions(List<SurveyQuestionSubmit> submits);
 
+    /**
+     * 加载问卷链接
+     * @param activity 问卷id
+     */
     SurveyHref loadSurveyHref(Integer activity);
 
+    /**
+     * 获取重定向地址
+     */
     String getRedirectUrl(String openId, Integer activity);
 }
