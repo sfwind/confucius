14111a0dacb1a0bc7f163a6779e82273b9fce065
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-09-15 22:48:15 +0800

    update

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/KnowledgeImportController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/KnowledgeImportController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/pc/admin/knowledge")
+@RequestMapping("/pc/operation/knowledge")
 public class KnowledgeImportController {
 
     @Autowired
