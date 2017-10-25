77fb3c08e200f0bcf5100b392d1ff4af327d1789
nethunder <17089113140@163.com>
2017-10-25 15:41:40 +0800

    update

--- confucius-web/src/main/java/com/iquanwai/confucius/web/course/dto/MonthlyCampDto.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/course/dto/MonthlyCampDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MonthlyCampDto {
-
+    private String marKSellingMemo; // 打点用到的，2017-10
     private Integer currentCampMonth; // 当前小课训练营配置月份
     private Integer campMonthProblemId; // 当前小课训练营跳转月份
 
