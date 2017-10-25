b0c8afcdc3a779b844603b696a026dbd0ef258a0
nethunder <17089113140@163.com>
2017-10-25 19:05:39 +0800

    update

--- confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/SignupController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/SignupController.java
@@ -476,7 +476,7 @@ public class SignupController {
         MonthlyCampDto dto = new MonthlyCampDto();
         MonthlyCampConfig monthlyCampConfig = cacheService.loadMonthlyCampConfig();
         Integer currentCampMonth = signupService.loadCurrentCampMonth(monthlyCampConfig);
-        dto.setMarKSellingMemo(monthlyCampConfig.getSellingYear() + "" + monthlyCampConfig.getSellingMonth());
+        dto.setMarKSellingMemo(monthlyCampConfig.getSellingYear() + "-" + monthlyCampConfig.getSellingMonth());
         dto.setCurrentCampMonth(currentCampMonth);
         dto.setCampMonthProblemId(signupService.loadHrefProblemId(currentCampMonth));
         return WebUtils.result(dto);
