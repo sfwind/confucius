fd8b7ce3153974f37f97d1372b7a44376e97afa8
xfduan <1047210087@qq.com>
2017-11-23 15:31:46 +0800

    fix accessToken

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/accesstoken/AccessTokenServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/accesstoken/AccessTokenServiceImpl.java
@@ -53,11 +53,11 @@ public class AccessTokenServiceImpl implements AccessTokenService {
         if(force) {
             forceUpdateAccessToken();
         }else{
-            String token = redisUtil.get("accesstoken");
+            String token = redisUtil.get("accessToken");
             if(token==null){
                 logger.info("insert access token");
                 String accessToken = _getAccessToken();
-                redisUtil.set("accesstoken", accessToken);
+                redisUtil.set("accessToken", accessToken);
                 accessTokenDao.insertOrUpdate(accessToken);
             }else{
                 //如果数据库的accessToken未刷新,则强制刷新
@@ -76,7 +76,7 @@ public class AccessTokenServiceImpl implements AccessTokenService {
 
     private void forceUpdateAccessToken(){
         String accessToken = _getAccessToken();
-        redisUtil.set("accesstoken", accessToken);
+        redisUtil.set("accessToken", accessToken);
         accessTokenDao.insertOrUpdate(accessToken);
     }
 }
