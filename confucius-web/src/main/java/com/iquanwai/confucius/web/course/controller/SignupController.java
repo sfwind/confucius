6c552b5acfc8353fbe0e6b724af080fa804c94aa
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-11-29 20:50:17 +0800

    fix

--- confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/SignupController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/course/controller/SignupController.java
@@ -366,7 +366,7 @@ public class SignupController {
 
         BusinessSchool bs = signupService.getSchoolInfoForPay(loginUser.getId());
         if (QuanwaiOrder.FRAG_MEMBER.equals(goodsInfoDto.getGoodsType()) && !bs.getIsBusinessStudent()) {
-            goodsInfoDto.setInitPrice(bs.getFee());
+//            goodsInfoDto.setInitPrice(bs.getFee());
             goodsInfoDto.setFee(bs.getFee());
         }
 
