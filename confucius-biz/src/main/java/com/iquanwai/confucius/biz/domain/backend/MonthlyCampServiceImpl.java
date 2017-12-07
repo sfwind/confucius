4e2ae2f663b7cee218199372e9961db9547f782e
xfduan <1047210087@qq.com>
2017-12-07 15:09:19 +0800

    add point logger

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/backend/MonthlyCampServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/backend/MonthlyCampServiceImpl.java
@@ -99,10 +99,14 @@ public class MonthlyCampServiceImpl implements MonthlyCampService {
 
         // 获取切换之前月份的所有人员
         List<RiseClassMember> sourceRiseClassMembers = riseClassMemberDao.loadAllByYearMonth(sourceYear, sourceMonth);
+        logger.info("point1");
         List<RiseMember> sourceRiseMembers = riseMemberDao.loadByProfileIds(sourceRiseClassMembers.stream().map(RiseClassMember::getProfileId).collect(Collectors.toList()));
+        logger.info("point2");
         Map<Integer, RiseMember> sourceRiseMemberMap = sourceRiseMembers.stream().collect(Collectors.toMap(RiseMember::getProfileId, riseMember -> riseMember));
+        logger.info("point3");
 
         sourceRiseClassMembers.forEach(riseClassMember -> {
+            logger.info("正在处理:" + riseClassMember.getMemberId());
             // 筛选出其中的仍是商学院的人员
             Integer profileId = riseClassMember.getProfileId();
             RiseMember riseMember = sourceRiseMemberMap.get(profileId);
