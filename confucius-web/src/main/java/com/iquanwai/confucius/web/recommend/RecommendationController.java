ba74169bb514181c08c622ddb7c30db538865471
richardyangcz <richardyangcz@sina.com>
2017-12-13 12:08:42 +0600

    add operation log

--- confucius-web/src/main/java/com/iquanwai/confucius/web/recommend/RecommendationController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/recommend/RecommendationController.java
@@ -1,7 +1,9 @@
 package com.iquanwai.confucius.web.recommend;
 
 import com.iquanwai.confucius.biz.domain.fragmentation.recommedation.RecommedationService;
+import com.iquanwai.confucius.biz.domain.log.OperationLogService;
 import com.iquanwai.confucius.biz.domain.weixin.account.AccountService;
+import com.iquanwai.confucius.biz.po.OperationLog;
 import com.iquanwai.confucius.biz.po.common.customer.Profile;
 import com.iquanwai.confucius.web.resolver.LoginUser;
 import com.iquanwai.confucius.web.util.WebUtils;
@@ -24,27 +26,45 @@ public class RecommendationController {
     private AccountService accountService;
 
     @Autowired
+    private OperationLogService operationLogService;
+
+    @Autowired
     private RecommedationService recommedationService;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
      * 获得当前用户的riseId
+     *
      * @param loginUser
      * @return
      */
     @RequestMapping("/get/rise")
-    public ResponseEntity<Map<String,Object>> getRise(LoginUser loginUser){
-        Assert.notNull(loginUser,"用户不能为空");
+    public ResponseEntity<Map<String, Object>> getRise(LoginUser loginUser) {
+        Assert.notNull(loginUser, "用户不能为空");
         Profile profile = accountService.getProfile(loginUser.getOpenId());
         logger.info("进入获得当前用户");
+
+        OperationLog operationLog = OperationLog.create().openid(loginUser.getOpenId())
+                .module("用户推荐")
+                .function("获得当前用户")
+                .action("获得当前用户riseId");
+        operationLogService.log(operationLog);
+
         return WebUtils.result(profile.getRiseId());
     }
 
     @RequestMapping("/add/user/{riseId}")
-    public ResponseEntity<Map<String,Object>> addUserRecommendation(LoginUser loginUser,@PathVariable String riseId){
-        Assert.notNull(loginUser,"用户不能为空");
+    public ResponseEntity<Map<String, Object>> addUserRecommendation(LoginUser loginUser, @PathVariable String riseId) {
+        Assert.notNull(loginUser, "用户不能为空");
         String openId = loginUser.getOpenId();
-        return WebUtils.result(recommedationService.addUserRecommedation(openId,riseId));
+
+        OperationLog operationLog = OperationLog.create().openid(openId)
+                .module("用户推荐")
+                .function("添加用户推荐")
+                .action("添加用户推荐信息");
+        operationLogService.log(operationLog);
+
+        return WebUtils.result(recommedationService.addUserRecommedation(openId, riseId));
     }
 }
