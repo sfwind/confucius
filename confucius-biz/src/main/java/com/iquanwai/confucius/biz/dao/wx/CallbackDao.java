7d24ffd5092c02e26a622521d30f49e7a2b54791
xfduan <1047210087@qq.com>
2018-01-23 00:45:04 +0800

    fix sql

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/wx/CallbackDao.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/wx/CallbackDao.java
@@ -51,7 +51,7 @@ public class CallbackDao extends DBUtil {
         QueryRunner run = new QueryRunner(getDataSource());
         try {
             run.update("UPDATE Callback Set AccessToken = ?, RefreshToken = ?, Openid = ?, UnionId = ? where State = ?",
-                    accessToken, refreshToken, openid, state, unionId);
+                    accessToken, refreshToken, openid, unionId, state);
 
         } catch (SQLException e) {
             logger.error(e.getLocalizedMessage(), e);
