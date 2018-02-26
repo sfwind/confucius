07e2e094502cc917d1971dc3365bd4caa3598c81
richardyangcz <richardyangcz@sina.com>
2018-02-26 11:06:01 +0600

    fix

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/po/common/permisson/Role.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/po/common/permisson/Role.java
@@ -18,17 +18,26 @@ public class Role {
     public static final Integer STRANGE = 0; //陌生人
     public static final Integer STUDENT = 1; //普通用户
     public static final Integer EMPLOYEE = 2; //工作人员
-    public static final Integer COACH_LEVEL_1 = 3; //见习教练
-    public static final Integer COACH_LEVEL_2 = 4; //教练
-    public static final Integer COACH_LEVEL_3 = 5; //高级教练
-    public static final Integer COACH_LEVEL_4 = 6; //首席教练
+//    public static final Integer COACH_LEVEL_1 = 3; //见习教练
+//    public static final Integer COACH_LEVEL_2 = 4; //教练
+//    public static final Integer COACH_LEVEL_3 = 5; //高级教练
+//    public static final Integer COACH_LEVEL_4 = 6; //首席教练
+    public static final Integer COACH_LEVEL_1 = 3;//见习
+    public static final Integer COACH_LEVEL_2 = 4;//I级教练
+    public static final Integer COACH_LEVEL_3 = 12;//II级教练
+    public static final Integer COACH_LEVEL_4 = 13;//III级教练
+    public static final Integer COACH_LEVEL_5 = 5;//高级教练1
+    public static final Integer COACH_LEVEL_6 = 14;//高级教练2
+    public static final Integer COACH_LEVEL_7 = 15;//高级教练3
+    public static final Integer COACH_LEVEL_8 = 6;//首席教练
+
     public static final Integer BIG_VIP = 7; //大V
     public static final Integer CONTENT_ADMIN = 8; //内容管理员
     public static final Integer ADMIN = 9; //管理员
     public static final Integer CONTENT_OPERATION = 10; //内容运营
 
     private static List<Integer> asstRoleLevel = Lists.newArrayList(COACH_LEVEL_1, COACH_LEVEL_2,
-            COACH_LEVEL_3, COACH_LEVEL_4, CONTENT_OPERATION, CONTENT_ADMIN);
+            COACH_LEVEL_3, COACH_LEVEL_4, COACH_LEVEL_4,COACH_LEVEL_5,COACH_LEVEL_6,COACH_LEVEL_7,COACH_LEVEL_8,CONTENT_OPERATION, CONTENT_ADMIN);
 
     public static Role stranger(){
         Role stranger = new Role();
