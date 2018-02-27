77c76b22ec75296b9ec39a6ec5289917c1ade805
richardyangcz <richardyangcz@sina.com>
2018-02-27 11:26:56 +0600

    add User Info

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/UserController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.iquanwai.confucius.biz.po.OperationLog;
 import com.iquanwai.confucius.biz.po.common.customer.Profile;
 import com.iquanwai.confucius.biz.po.fragmentation.RiseClassMember;
 import com.iquanwai.confucius.biz.po.fragmentation.RiseMember;
+import com.iquanwai.confucius.biz.util.DataUtils;
 import com.iquanwai.confucius.web.pc.backend.dto.UserDto;
 import com.iquanwai.confucius.web.resolver.UnionUser;
 import com.iquanwai.confucius.web.util.WebUtils;
@@ -34,19 +35,19 @@ public class UserController {
 
     /**
      * 查询用户信息接口
-     * @param type
+     *
      * @param search
      * @return
      */
     @RequestMapping("/search")
-    public ResponseEntity<Map<String,Object>> searchUserInfo(UnionUser unionUser,@RequestParam("type")Integer type, @RequestParam("search")String search){
+    public ResponseEntity<Map<String, Object>> searchUserInfo(UnionUser unionUser, @RequestParam("search") String search) {
 
         OperationLog operationLog = OperationLog.create().openid(unionUser.getOpenId()).module("内容运营").action("用户信息").action("查询用户信息");
         operationLogService.log(operationLog);
 
-        UserDto userDto = generateUserDto(type,search);
+        UserDto userDto = generateUserDto(search);
 
-        if(userDto==null){
+        if (userDto == null) {
             return WebUtils.error("找不到该用户");
         }
         return WebUtils.result(userDto);
@@ -55,30 +56,31 @@ public class UserController {
 
     /**
      * 生成用户信息
-     * @param type
+     *
      * @param search
      * @return
      */
-    private UserDto generateUserDto(Integer type,String search){
+    private UserDto generateUserDto(String search) {
         UserDto userDto = new UserDto();
-        Profile profile = null;
-        if(memberType.equals(type)){
-            profile = accountService.loadProfileByMemberId(search);
-        }else if(profileIdType.equals(type)){
-            profile = accountService.getProfile(Integer.valueOf(search));
-        }else if(riseIdType.equals(type)){
-           profile = accountService.getProfileByRiseId(search);
+        Profile profile = accountService.loadProfileByMemberId(search);
+        if (profile == null) {
+            profile = accountService.getProfileByRiseId(search);
         }
-        if(profile==null){
+        if (profile == null) {
+            if (DataUtils.isInteger(search)) {
+                profile = accountService.getProfile(Integer.valueOf(search));
+            }
+        }
+        if (profile == null) {
             return null;
         }
-        BeanUtils.copyProperties(profile,userDto);
+        BeanUtils.copyProperties(profile, userDto);
         RiseClassMember riseClassMember = accountService.getLatestMemberId(profile.getId());
-        if(riseClassMember!=null){
+        if (riseClassMember != null) {
             userDto.setMemberId(riseClassMember.getMemberId());
         }
         RiseMember riseMember = accountService.getCurrentRiseMember(profile.getId());
-        if(riseMember!=null){
+        if (riseMember != null) {
             userDto.setOpenDate(riseMember.getOpenDate());
             userDto.setMemberType(riseMember.getMemberTypeId());
         }
