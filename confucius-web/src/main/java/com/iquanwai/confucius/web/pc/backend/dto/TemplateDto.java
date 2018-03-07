5ab967a0c5015122b4739d0e05f493c9e74af215
richardyangcz <richardyangcz@sina.com>
2018-03-07 18:55:51 +0600

    add template

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/dto/TemplateDto.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/dto/TemplateDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @Data
 public class TemplateDto {
-    private String templateId;
+    private Integer templateId;
     private String comment;
     private String first;
     private String remark;
