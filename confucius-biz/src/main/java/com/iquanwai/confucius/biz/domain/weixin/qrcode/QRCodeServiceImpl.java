d84f469e4da47bd21c1131913393c73ee85083d7
richardyangcz <richardyangcz@sina.com>
2018-03-07 11:51:19 +0600

    add logger

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/qrcode/QRCodeServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/weixin/qrcode/QRCodeServiceImpl.java
@@ -107,10 +107,10 @@ public class QRCodeServiceImpl implements QRCodeService {
             promotionQrCode.setRemark("123");
 
             logger.info("开始上传文件");
-//            boolean isSuccess = QiNiuUtils.uploadFile("test-123.jpeg", inputStream);
-//            if (isSuccess) {
-//                promotionQrCode.setUrl("hello");
-//            }
+            boolean isSuccess = QiNiuUtils.uploadFile("challenge-20180307115220-td5joddb2.jpeg", inputStream);
+            if (isSuccess) {
+                promotionQrCode.setUrl("hello");
+            }
             logger.info("上传文件成功");
 
 //            logger.info("开始上传文件");
