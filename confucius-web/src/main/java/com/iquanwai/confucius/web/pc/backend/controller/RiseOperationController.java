cf022aba586f76f58da6af6e912322449660be42
richardyangcz <richardyangcz@sina.com>
2018-03-08 11:15:41 +0600

    test

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/RiseOperationController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/RiseOperationController.java
@@ -512,7 +512,7 @@ public class RiseOperationController {
                 if (first.contains("{username}")) {
                     first = replaceNickname(openid, first);
                 }
-                data.put("first", new TemplateMessage.Keyword(first));
+                data.put("first", new TemplateMessage.Keyword(first+"\\n"));
             }
             if (templateDto.getKeyword1() != null) {
                 String keyword1 = templateDto.getKeyword1();
