eb6fce9829c5072c0bf9a8fb1e6c92b126edf9af
nethunder <17089113140@163.com>
2018-03-28 09:52:11 +0800

    reback school video page

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
@@ -95,11 +95,11 @@ public class OperationLogServiceImpl implements OperationLogService {
                 properties.put("riseId", profile.getRiseId());
 
                 sa.track(profileId.toString(), true, eventName, properties);
+                // TODO 上线前删掉
+                sa.flush();
             } catch (InvalidArgumentException e) {
                 logger.error(e.getLocalizedMessage(), e);
             }
-            // TODO 上线前删掉
-            sa.flush();
         });
     }
 
