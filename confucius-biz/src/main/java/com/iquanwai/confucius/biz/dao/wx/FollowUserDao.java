6deaca129688be1ab5235dd26e82af5838e326ea
Ding Zhi Jun <dingzhijun@imdada.cn>
2018-03-28 22:49:35 +0800

    remove

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/wx/FollowUserDao.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/dao/wx/FollowUserDao.java
@@ -1,19 +1,16 @@
 package com.iquanwai.confucius.biz.dao.wx;
 
-import com.google.common.collect.Lists;
 import com.iquanwai.confucius.biz.dao.DBUtil;
 import com.iquanwai.confucius.biz.po.Account;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.ColumnListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
-import java.util.List;
 
 /**
  * Created by justin on 16/8/12.
@@ -88,48 +85,6 @@ public class FollowUserDao extends DBUtil {
             logger.error(e.getLocalizedMessage(), e);
         }
         return null;
-    }
-
-    public List<String> queryAll() {
-        QueryRunner run = new QueryRunner(getDataSource());
-        ResultSetHandler<List<String>> h = new ColumnListHandler<>();
-
-        try {
-            List<String> account = run.query("SELECT OpenId FROM FollowUsers", h);
-            return account;
-        } catch (SQLException e) {
-            logger.error(e.getLocalizedMessage(), e);
-        }
-
-        return Lists.newArrayList();
-    }
-
-    public void updateMeta(Account account) {
-        QueryRunner run = new QueryRunner(getDataSource());
-        String updateSql = "Update FollowUsers Set Nickname=?, Headimgurl=?, UnionId=?, Subscribe=? where Openid=?";
-        try {
-            run.update(updateSql,
-                    account.getNickname(), account.getHeadimgurl(),
-                    account.getUnionid(), account.getSubscribe(), account.getOpenid());
-        } catch (SQLException e) {
-            logger.error(e.getLocalizedMessage(), e);
-        }
-    }
-
-    public void updateInfo(Account account) {
-        QueryRunner run = new QueryRunner(getDataSource());
-        String updateSql = "Update FollowUsers Set MobileNo=?, Email=?, Industry=?, Function=?, WorkingLife=?, " +
-                "RealName=?, City=?, Province=? where Openid=?";
-        try {
-            run.update(updateSql,
-                    account.getMobileNo(), account.getEmail(),
-                    account.getIndustry(), account.getFunction(),
-                    account.getWorkingLife(), account.getRealName(),
-                    account.getCity(), account.getProvince(),
-                    account.getOpenid());
-        } catch (SQLException e) {
-            logger.error(e.getLocalizedMessage(), e);
-        }
     }
 
     public void unsubscribe(String openid) {
