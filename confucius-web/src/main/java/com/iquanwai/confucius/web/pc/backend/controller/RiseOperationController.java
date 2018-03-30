893faa04e80ae1c5702cd780776ac14dfd2ce78e
nethunder <17089113140@163.com>
2018-03-30 16:10:06 +0800

    update zk

--- confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/RiseOperationController.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/pc/backend/controller/RiseOperationController.java
@@ -405,6 +405,7 @@ public class RiseOperationController {
                             if (approveDto.getInterviewDto().getInterviewerId() != null) {
                                 prop.add("auditor", approveDto.getInterviewDto().getInterviewerId());
                             }
+                            prop.add("coupon", approveDto.getCoupon() != null ? approveDto.getCoupon() : 0);
                             return prop;
                         });
 
