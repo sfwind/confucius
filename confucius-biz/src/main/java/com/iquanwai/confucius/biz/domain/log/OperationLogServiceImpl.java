680a738651a46e1e4cfc3cdfb4f4cd42e7bc5d77
nethunder <17089113140@163.com>
2018-04-03 11:37:03 +0800

    update zk

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
@@ -95,8 +95,8 @@ public class OperationLogServiceImpl implements OperationLogService {
                 properties.put("riseId", profile.getRiseId());
                 logger.info("trace:\nprofielId:{}\neventName:{}\nprops:{}", profileId, eventName, properties);
                 sa.track(profile.getRiseId(), true, eventName, properties);
-                // TODO 上线前删掉
-                sa.flush();
+                //  上线前删掉
+//                sa.flush();
             } catch (InvalidArgumentException e) {
                 logger.error(e.getLocalizedMessage(), e);
             }
