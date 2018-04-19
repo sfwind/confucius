e0109ad70d54c57170b061bf37741dd3d1577fdb
三十文 <1047210087@qq.com>
2018-04-19 11:31:27 +0800

    fix

--- confucius-web/src/main/java/com/iquanwai/confucius/web/interceptor/HandlerInterceptor.java
+++ confucius-web/src/main/java/com/iquanwai/confucius/web/interceptor/HandlerInterceptor.java
@@ -35,7 +35,7 @@ public class HandlerInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         UnionUser.Platform platform = unionUserService.getPlatformType(request);
-        if (platform == null || unionUserService.isDocumentRequest(request)) {
+        if ((platform == null || unionUserService.isDocumentRequest(request)) && !unionUserService.isInterceptorRequestURI(request)) {
             return true;
         } else {
             Callback callback = unionUserService.getCallbackByRequest(request);
