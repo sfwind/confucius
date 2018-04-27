8a75f17ca0061a5242f8be3e3fd51ef666e4ef8d
nethunder <17089113140@163.com>
2018-04-28 01:49:10 +0800

    更新point

--- confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
+++ confucius-biz/src/main/java/com/iquanwai/confucius/biz/domain/log/OperationLogServiceImpl.java
@@ -9,14 +9,18 @@ import com.iquanwai.confucius.biz.dao.common.log.OperationLogDao;
 import com.iquanwai.confucius.biz.dao.common.permission.UserRoleDao;
 import com.iquanwai.confucius.biz.dao.fragmentation.FragmentClassMemberDao;
 import com.iquanwai.confucius.biz.dao.fragmentation.RiseClassMemberDao;
+import com.iquanwai.confucius.biz.dao.quanwai.EmployeeDao;
 import com.iquanwai.confucius.biz.domain.course.signup.RiseMemberManager;
 import com.iquanwai.confucius.biz.domain.course.signup.RiseMemberTypeRepo;
 import com.iquanwai.confucius.biz.domain.fragmentation.ClassMember;
 import com.iquanwai.confucius.biz.po.ActionLog;
 import com.iquanwai.confucius.biz.po.OperationLog;
 import com.iquanwai.confucius.biz.po.common.customer.Profile;
+import com.iquanwai.confucius.biz.po.common.permisson.Role;
 import com.iquanwai.confucius.biz.po.common.permisson.UserRole;
+import com.iquanwai.confucius.biz.po.fragmentation.RiseClassMember;
 import com.iquanwai.confucius.biz.po.fragmentation.RiseMember;
+import com.iquanwai.confucius.biz.po.quanwai.QuanwaiEmployee;
 import com.iquanwai.confucius.biz.util.ConfigUtils;
 import com.iquanwai.confucius.biz.util.ThreadPool;
 import com.sensorsdata.analytics.javasdk.SensorsAnalytics;
@@ -25,6 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import reactor.core.support.Assert;
 
 import javax.annotation.PostConstruct;
@@ -57,6 +62,8 @@ public class OperationLogServiceImpl implements OperationLogService {
     private FragmentClassMemberDao fragmentClassMemberDao;
     @Autowired
     private RiseMemberTypeRepo riseMemberTypeRepo;
+    @Autowired
+    private EmployeeDao employeeDao;
 
 
     private Map<Integer, String> classNameMap = Maps.newHashMap();
@@ -116,19 +123,20 @@ public class OperationLogServiceImpl implements OperationLogService {
                 }
 
                 List<ClassMember> classMembers = fragmentClassMemberDao.loadActiveByProfileId(profileId);
-                if (classMembers == null) {
-                    classMembers = Lists.newArrayList(fragmentClassMemberDao.loadLatestByProfileId(profileId));
+                if (CollectionUtils.isEmpty(classMembers)) {
+                    ClassMember classMember = fragmentClassMemberDao.loadLatestByProfileId(profileId);
+                    if (classMember != null) {
+                        classMembers.add(classMember);
+                    }
                 }
-                if (!classMembers.isEmpty()) {
-                    classMembers.forEach(item -> {
-                        if (item.getClassName() != null) {
-                            properties.put(classNameMap.get(item.getMemberTypeId()), item.getClassName());
-                        }
-                        if (item.getGroupId() != null) {
-                            properties.put(groupIdMap.get(item.getMemberTypeId()), item.getGroupId());
-                        }
-                    });
-                }
+                classMembers.forEach(item -> {
+                    if (item.getClassName() != null) {
+                        properties.put(classNameMap.get(item.getMemberTypeId()), item.getClassName());
+                    }
+                    if (item.getGroupId() != null) {
+                        properties.put(groupIdMap.get(item.getMemberTypeId()), item.getGroupId());
+                    }
+                });
                 properties.put("isAsst", role != null);
                 properties.put("riseId", profile.getRiseId());
                 logger.info("trace:\nprofielId:{}\neventName:{}\nprops:{}", profileId, eventName, properties);
@@ -184,5 +192,71 @@ public class OperationLogServiceImpl implements OperationLogService {
                 logger.error(e.getLocalizedMessage(), e);
             }
         });
+    }
+
+    @Override
+    public void refreshProfiles(List<Integer> profileIds) {
+        // 员工
+        Map<Integer, List<QuanwaiEmployee>> employeeRepo = employeeDao.loadAll(QuanwaiEmployee.class).stream().collect(Collectors.groupingBy(QuanwaiEmployee::getProfileId));
+        // vip
+        Map<Integer, List<UserRole>> roleRepo = userRoleDao.loadAll(UserRole.class).stream().filter(item -> !item.getDel()).collect(Collectors.groupingBy(UserRole::getProfileId));
+        // 会员
+        Map<Integer, List<RiseMember>> riseMemberRepo = riseMemberDao.loadByProfileIds(profileIds).stream().filter(item -> !item.getMemberTypeId().equals(RiseMember.COURSE)).collect(Collectors.groupingBy(RiseMember::getProfileId));
+        // 身份信息
+        Map<Integer, List<Profile>> profileRepo = profileDao.loadByProfileIds(profileIds).stream().collect(Collectors.groupingBy(Profile::getId));
+
+        profileIds.forEach(profileId -> ThreadPool.execute(() -> {
+            profileRepo.get(profileId).stream().findFirst().ifPresent(profile -> {
+                Prop prop = OperationLogService.props();
+                // roleNames,openid,classname,gropuid,isAsst,isEmplyee
+                List<RiseMember> members = riseMemberRepo.get(profileId);
+
+                if (!CollectionUtils.isEmpty(members)) {
+                    prop.add("roleNames", members.stream().map(RiseMember::getMemberTypeId).map(String::valueOf).collect(Collectors.toList()));
+                }
+                prop.add("openId", profile.getOpenid());
+                List<ClassMember> classMembers = fragmentClassMemberDao.loadByProfileId(profileId);
+                if (!CollectionUtils.isEmpty(classMembers)) {
+                    classMembers.stream().collect(Collectors.groupingBy(ClassMember::getMemberTypeId)).forEach((memberId, list) -> {
+                        list.stream().sorted(((o1, o2) -> o2.getId() - o1.getId())).findFirst().ifPresent(item -> {
+                            if (item.getClassName() != null) {
+                                prop.add("className_" + item.getMemberTypeId(), item.getClassName());
+                            }
+                            if (item.getGroupId() != null) {
+                                prop.add("groupId_" + item.getMemberTypeId(), item.getGroupId());
+                            }
+                        });
+                    });
+                } else {
+                    RiseClassMember riseClassMember = riseClassMemberDao.loadLatestRiseClassMember(profileId);
+                    if (riseClassMember != null) {
+                        if (riseClassMember.getClassName() != null) {
+                            Integer memberTypeId;
+                            if (Integer.parseInt(riseClassMember.getClassName()) % 2 == 0) {
+                                memberTypeId = 5;
+                            } else {
+                                memberTypeId = 3;
+                            }
+                            prop.add("className_" + memberTypeId, riseClassMember.getClassName());
+                            if (riseClassMember.getGroupId() != null) {
+                                prop.add("groupId_" + memberTypeId, riseClassMember.getGroupId());
+                            }
+                        }
+                    }
+                }
+                roleRepo.get(profileId).stream().filter(item -> Role.isAsst(item.getRoleId())).findFirst().ifPresent(item -> prop.add("isAsst", true));
+                employeeRepo.get(profileId).stream().findFirst().ifPresent(item -> prop.add("employee", true));
+                Map<String, Object> properties = prop.build();
+                logger.info("trace:\nprofielId:{}\neventName:{}\nprops:{}", profileId, "profileSet", properties);
+                try {
+                    sa.profileSet(profile.getRiseId(), true, properties);
+                    if (ConfigUtils.isDevelopment()) {
+                        sa.flush();
+                    }
+                } catch (InvalidArgumentException e) {
+                    logger.error(e.getLocalizedMessage(), e);
+                }
+            });
+        }));
     }
 }
